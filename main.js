7a8741e59b860946b5e0b98a6fe661f9bccb5d42
mih-8 <mirian.calistro.souza@escola.pr.gov.br>
2025-06-13 12:08:11 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+const mario = document.querySelector(".mario")
+const pipe = document.querySelector(".pipe")
 
+const jump = () => {
+  mario.classaList.remove("jump");
+
+  setTimeout(() => { 
+       mario.classList.remover("jump");
+  }
+    ,500);
+}
+const loop = sentIntervall(() => {
+const pipePosition = pipe.offsetLeft;
+const marioPosition = +window.getComputedStyle(mario).bottom.replace("px","");
