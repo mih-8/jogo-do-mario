185d9fa725bcc19be7714f70e154e8a201accb9c
mih-8 <mirian.calistro.souza@escola.pr.gov.br>
2025-06-13 14:47:09 -0300

    Update main.js

--- main.js
+++ main.js
@@ -9,6 +9,17 @@ const jump = () => {
   }
     ,500);
 }
+
 const loop = sentIntervall(() => {
 const pipePosition = pipe.offsetLeft;
 const marioPosition = +window.getComputedStyle(mario).bottom.replace("px","");
+
+  if (pipePosition <= 120 && pipePosion > 0 && marioPositon < 80) {
+      pipe.style.animation = "none";
+      pipe.style.left = ${pipePosition}px ;
+
+      mario.style.animation = "none";
+      mario.style.left = ${pipePosition}px ;
+   }   
+})
+document.addEventListener("keydow", jump);
